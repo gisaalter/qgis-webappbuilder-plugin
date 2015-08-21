679b573c4cb0db0ea79405a8e0cec2d52699ef16
volaya <volayaf@gmail.com>
2015-08-21 14:24:23 +0200

    updated examples

---
+++ examples/full/resources/addlayer.js
@@ -0,0 +1,139 @@
+
+var addLayerFromFile = function(){
+
+    var readFeatures = function(text){
+        var formats = [new ol.format.GeoJSON(), new ol.format.KML(), new ol.format.GPX()]
+        for(var i=0, len=formats.length; i< len; i++){
+            var format = formats[i];
+            try {
+                var crs = format.readProjection(text);
+                var features = format.readFeatures(text,
+                        {dataProjection: crs.getCode(),
+                        featureProjection: map.getView().getProjection().getCode()});
+                return features;
+            } catch (e) {}
+        }
+        return null;
+    }
+
+    var _addLayerFromFile = function(f, color, fillColor){
+        if (f) {
+            var r = new FileReader();
+            r.onload = function(e) {
+                var contents = e.target.result;
+                var features = readFeatures(contents);
+                if (features){
+                    var lyr = new ol.layer.Vector({
+                        source:  new ol.source.Vector({
+                                    features: features
+                                }),
+                        title: f.name,
+                        isRemovable: true,
+                        isSelectable: true
+                    });
+                    var style = function(feature, resolution) {
+                        var selected = lyr.selectedFeatures;
+                        if (selected && selected.indexOf(feature) != -1) {
+                            return [new ol.style.Style({
+                                        fill: new ol.style.Fill({
+                                            color: "rgba(255, 204, 0, 1)",
+                                        }),
+                                        stroke: new ol.style.Stroke({
+                                            color: "rgba(255, 204, 0, 1)",
+                                            width: 2
+                                        }),
+                                        image: new ol.style.Circle({
+                                            radius: 7,
+                                            fill: new ol.style.Fill({
+                                                color: "rgba(255, 204, 0, 1)",
+                                            })
+                                        })
+                                    })];
+                        }
+                        else{
+                            return [new ol.style.Style({
+                                        fill: new ol.style.Fill({
+                                            color: fillColor
+                                        }),
+                                        stroke: new ol.style.Stroke({
+                                            color: color,
+                                            width: 2
+                                        }),
+                                        image: new ol.style.Circle({
+                                            radius: 7,
+                                            fill: new ol.style.Fill({
+                                                color: color
+                                            })
+                                        })
+                                    })];
+                        }
+                    };
+                    lyr.setStyle(style);
+                    map.addLayer(lyr);
+                }
+                else{
+                    $("html").css("cursor", "default");
+                    alert("Failed to load file");
+                }
+            }
+            r.readAsText(f);
+        } else {
+            alert("Failed to load file");
+        }
+    };
+
+    var html = '<div class="row">  ' +
+            '<div class="col-md-12"> ' +
+            '<form class="form-horizontal"> ' +
+            '<div class="form-group"> ' +
+            '<label class="col-md-4 control-label" for="new-layer-file-textbox"> File </label>'+
+            '<div class="col-md-8">'  +
+            '<div class="input-group">' +
+            '<input id="new-layer-file-selector" type="file" accept=".geojson, .gpx, .kml" style="display:none">' +
+            '<input id="new-layer-file-textbox" class="form-control" type="text">' +
+            '<div class="input-group-addon">'+
+            '<a href="#" onclick="$(\'input[id=new-layer-file-selector]\').click();">Browse</a>' +
+            '</div></div>' +
+            '</div></div>'+
+            '<div class="form-group"> ' +
+            '<label class="col-md-4 control-label" for="new-layer-color"> Color </label>'+
+            '<div class="col-md-8"> <input value="#000000" id="new-layer-color" type="text" class="color-picker form-control">' +
+            '</div></div>'+
+            '<div class="form-group"> ' +
+            '<label class="col-md-4 control-label" for="new-layer-fillcolor"> Fill color (polygons only) </label>'+
+            '<div class="col-md-8"> <input value="#0000aa" id="new-layer-fillcolor" type="text" class="color-picker form-control">' +
+            '</div></div>'+
+            '</form></div></div>';
+
+    var dialog = bootbox.dialog({
+        title: "Create empty layer",
+        message: html,
+        buttons: {
+            success: {
+                label: "Ok",
+                className: "btn-success",
+                callback: function () {
+                    var file = $("#new-layer-file-selector").prop("files")[0]
+                    var color = $("#new-layer-color").val().toString();
+                    var fillColor = $("#new-layer-fillcolor").val().toString();
+                    busyProcess(function(){_addLayerFromFile(file, color, fillColor)}, 500);
+                }
+            }
+        },
+        show: false
+    });
+
+    dialog.on('show.bs.modal', function(){
+        $(".color-picker").colorpicker();
+        $('input[id=new-layer-file-selector]').change(function() {
+            $('#new-layer-file-textbox').val($(this).val());
+        });
+    });
+
+    dialog.modal("show");
+
+
+
+    return false;
+
+};
---
+++ examples/full/styles/lakes.js
@@ -0,0 +1,41 @@
+var textStyleCache_lakes = {}
+var clusterStyleCache_lakes = {}
+var selectedClusterStyleCache_lakes = {}
+var style_lakes = function(feature, resolution) {
+    var selected = lyr_lakes.selectedFeatures;
+
+    if (feature.hide === true) {
+        return null;
+    }
+
+
+    var value = ""
+    var style = [new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: "rgba(0,0,0,1.0)",
+            lineDash: null,
+            width: 0
+        }),
+        fill: new ol.style.Fill({
+            color: "rgba(76,94,154,1.0)"
+        })
+    })];
+    var selectionStyle = [new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: "rgba(255, 204, 0, 1)",
+            lineDash: null,
+            width: 0
+        }),
+        fill: new ol.style.Fill({
+            color: "rgba(255, 204, 0, 1)"
+        })
+    })];
+    allStyles = [];
+
+    if (selected && selected.indexOf(feature) != -1) {
+        allStyles.push.apply(allStyles, selectionStyle);
+    } else {
+        allStyles.push.apply(allStyles, style);
+    }
+    return allStyles;
+};
